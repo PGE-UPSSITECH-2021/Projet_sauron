029d13268a142f2cbd48d6a088a3e3d4ca663c48
Alexandre Malgouyres <amalgouyres@gmail.com>
2022-02-09 14:36:36 +0100

    Implementation identification.

--- deplacement_robot/scripts/run_identification.py
+++ deplacement_robot/scripts/run_identification.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from StepReader import StepReader
-from useful_robot import rotation_between_vect, homogeneous_matrix_to_pose_msg
+from useful_robot import rotation_between_vect, homogeneous_matrix_to_pose_msg, get_fk, pose_msg_to_homogeneous_matrix
 import numpy as np
 import rospy
 import rospkg
 from deplacement_robot.srv import Robot_move, Robot_move_predef, Robot_set_state
 from communication.srv import identification
-from deplacement_robot.msg import Identification
+from deplacement_robot.msg import Identification, Trou_identification
 
 from  geometry_msgs.msg import Pose
 
@@ -15,12 +15,12 @@ from  geometry_msgs.msg import Pose
 # TODO Appel du service
 # TODO Message ROS
 
-def run_identification(plaque_pos, nom_plaque, step_folder, param_int=None, dist =  0.93):
+def run_identification(plaque_pos, nom_plaque, step_folder, diametres, global_picture, intrinsic=np.eye(3), dist =  0.93, seuil=100):
     # Lecture du fichier step et recuperation des trous
     cylinders_dict = get_cylinders(step_folder + "/" + str(nom_plaque) + ".stp")
 
     # Determination du chemin en fonction des trous
-    points = get_poses(cylinders_dict, plaque_pos, dist)
+    poses = get_poses(cylinders_dict, plaque_pos, dist)
 
     ######## Deplacement du robot ########
     # Service pour deplacer le robot a un point donne
@@ -30,36 +30,89 @@ def run_identification(plaque_pos, nom_plaque, step_folder, param_int=None, dist
     # Service pour effectuer l identification
     identification_srv = rospy.ServiceProxy("camera/identification", identification)
 
+    # Dictionnaire point 3D (x,y,z) : diametre
+    dict_point = {}
+
     move_parcking()
 
-    for p in points:
-        print(p)
+    result_msg = Identification()
+    res_points = []
 
+    # DEBUG
+    nbTrousTraite = 0
+
+    for pose in poses:
         if rospy.is_shutdown():
             exit()
         
-        resp1 = move_robot(p)
+        resp1 = move_robot(pose[0])
+
+        # Récupération de la postion de la caméra
+        pos_cam = pose_msg_to_homogeneous_matrix(get_fk())
+        # Point devant être pris en compte (2D, 3D)
+        points = []
+
+        for point in pose[1]:
+            # Passage du point dans le repère image
+            p = get_points_projection(intrinseques, extrinseques, point)
+            points.append[(p, point)]
+
+        res_identification = identification_srv()
+
+        for point in res_identification.points.points:
+            if rospy.is_shutdown():
+                exit()
+
+            # On traite que les trous qui ont le bon diametre
+            if point.type in diametres :
+                Pi = np.array([point.x, point.y])
+                for p in points:
+                    # On verifie si le trous est bien un de ceux dont on est en face
+                    Pc = np.array(p[0])
+                    if np.linalg.norm(Pi-Pc) <= seuil:
+                        dict_point[tuple(p[1])] = point.type
+                        # DEBUG
+                        nbTrousTraite += 1
+                        pr = Trou_identification()
+                        pr.x = p[1][0]
+                        pr.x = p[1][1]
+                        pr.diametre = point.type
+                        res_points.append(pr)
+                        break
+
         print("press enter")
         raw_input()
 
     if rospy.is_shutdown():
         exit()
 
+    # DEBUG
+    print("nbTrousTraite : " + str(nbTrousTraite))
+
+    if rospy.is_shutdown():
+        exit()
+
     move_parcking()
 
-    #TODO
-    return Identification()
+    return result_msg
+
+def get_points_projection(intrinsic, extrinsic, P0):
+    # [xi,yi,1] = 1/z*[intrinseque].[extrinseque].[point]
+    P0 = np.hstack((P0, 1))
+    Pc = np.dot(extrinsic,P0)
+    Pi = 1/Pc[2]*np.dot(intrinsic, Pc[:3])
+    pass
 
 def get_poses(cylinders_dict, plaque_pos, dist):
-    points = []
+    poses = []
 
     for key in cylinders_dict:
         cylinders = cylinders_dict[key]
-        pos = []
+        point = []
         for c in cylinders :
-            pos.append(c.position)
+            point.append(c.position)
 
-        m = np.mean(pos, 0)/1000
+        m = np.mean(point, 0)/1000
         m = np.dot(plaque_pos, np.hstack((m, 1)))[:3]
 
         p1 = m + dist * np.array(key)
@@ -79,9 +132,9 @@ def get_poses(cylinders_dict, plaque_pos, dist):
         p = np.hstack((r, p.T))
         p = np.vstack((p, [0,0,0,1]))
         msg = homogeneous_matrix_to_pose_msg(p)
-        points.append(msg)
+        poses.append((msg, point))
 
-    return points
+    return poses
 
 def get_cylinders(file_path):
     step = StepReader(file_path)
