9c88ce6cc49a11d30430c95e38f6762ce9cd78e2
Alexandre Malgouyres <amalgouyres@gmail.com>
2022-02-10 10:16:51 +0100

    Netoyage du fichier.

--- deplacement_robot/scripts/move_robot_server_demo_release4.py
+++ deplacement_robot/scripts/move_robot_server_demo_release4.py
@@ -56,7 +56,6 @@ class Move_robot:
         pose_goal = msg.Pose
         print("Move robot to : " + str(pose_goal))
 
-
         for i in range(5):
             self.group.set_pose_target(pose_goal)
 
@@ -64,8 +63,6 @@ class Move_robot:
 
             if plan.joint_trajectory.joint_names != [] :
                 break
-        
-        print(plan)
 
         if plan.joint_trajectory.joint_names == [] :
             print(False)
@@ -75,15 +72,6 @@ class Move_robot:
             self.group.stop()
             self.group.clear_pose_targets()
             return True
-
-    def handler_robot_move_home(self, a):
-        return self.move_predef("home")
-
-    def handler_robot_move_calibration(self, a):
-        return self.move_predef("calibration")
-
-    def handler_robot_move_localisation(self, a):
-        return self.move_predef("localisation")
 
     def handler_robot_move_parcking(self, a):
         return self.move_predef("parcking")
@@ -123,48 +111,13 @@ class Move_robot:
             self.group.clear_pose_targets()
             return True
 
-    def draw_square(self, a):
-        waypoints = []
-
-        wpose = self.group.get_current_pose().pose
-        wpose.position.x += 0.3
-        waypoints.append(copy.deepcopy(wpose))
-        wpose.position.y += 0.3
-        waypoints.append(copy.deepcopy(wpose))
-        wpose.position.x -= 0.3  
-        waypoints.append(copy.deepcopy(wpose))
-        wpose.position.y -= 0.3
-        waypoints.append(copy.deepcopy(wpose))
-
-        # We want the Cartesian path to be interpolated at a resolution of 1 cm
-        # which is why we will specify 0.01 as the eef_step in Cartesian
-        # translation.  We will disable the jump threshold by setting it to 0.0 disabling:
-        (plan, fraction) = self.group.compute_cartesian_path(
-                                        waypoints,   # waypoints to follow
-                                        0.01,        # eef_step
-                                        0.0)         # jump_threshold
-
-        # Note: We are just planning, not asking move_group to actually move the robot yet:
-        self.group.execute(plan, wait=True)
-
-        return []
-
     def move_picture(self, a):
-        self.state = "EN PRODUCTION"
         res = self.move_predef("picture")
-        self.state = "LIBRE NON INIT"
         return res
 
     def move_camera(self, a):
-        self.state = "EN PRODUCTION"
         res = self.move_predef("camera")
-        self.state = "LIBRE NON INIT"
         return res
-
-    def move_init(self, msg):
-        self.state = "INITIALISATION"
-        self.move_predef("parcking")
-        self.state = "LIBRE INIT"
 
     def set_state(self, msg):
         self.state = msg.state
@@ -174,20 +127,8 @@ class Move_robot:
         s = rospy.Service('move_robot', Robot_move, self.handler_robot_move)
         rospy.loginfo("Server move robot ready !")
 
-        s1 = rospy.Service('move_robot_home', Robot_move_predef, self.handler_robot_move_home)
-        rospy.loginfo("Server move robot to home ready !")
-
-        s2 = rospy.Service('move_robot_calibration', Robot_move_predef, self.handler_robot_move_calibration)
-        rospy.loginfo("Server move robot to calibration ready !")
-
-        s3 = rospy.Service('move_robot_localisation', Robot_move_predef, self.handler_robot_move_localisation)
-        rospy.loginfo("Server move robot to localisation ready !")
-
         s4 = rospy.Service('move_robot_parcking', Robot_move_predef, self.handler_robot_move_parcking)
         rospy.loginfo("Server move robot to parcking ready !")
-
-        s5 = rospy.Service('move_picture', Robot_move_predef, self.move_picture)
-        rospy.loginfo("Server move robot to picture ready !")
 
         s6 = rospy.Service('move_camera', Robot_move_predef, self.move_camera)
         rospy.loginfo("Server move robot to camera ready !")
@@ -199,16 +140,7 @@ class Move_robot:
 
         s_speed = rospy.Service('set_speed_percentage', Speed_percentage, self.handler_set_speed_perentage)
 
-        s_square = rospy.Service('move_square', Robot_do_square, self.draw_square)
-
-        s_init = rospy.Service("move_robot_init", Robot_move_predef, self.move_init)
-
         s_state = rospy.Service("set_robot_state", Robot_set_state, self.set_state)
-
-        s_en_const = rospy.Service("enable_const", Robot_move_predef, self.enable_constraint)
-        rospy.Service("disable_const", Robot_move_predef, self.remove_constraint)
-
-        s_test = rospy.Service("robot_test", Robot_move, self.test)
 
         rospy.loginfo("Robot ready to move !")
 
@@ -229,45 +161,6 @@ class Move_robot:
             self.group.stop()
             return True
 
-    def enable_constraint(self, msg):
-        constraints = Constraints()
-        joint_constraint = JointConstraint()
-
-        constraints.name = "joint_2_l"
-
-        joint_constraint.position = 0
-        joint_constraint.tolerance_above = 3.14/4
-        joint_constraint.tolerance_below = 3.14/4
-        joint_constraint.weight = 1
-
-        joint_constraint.joint_name = "joint_2_l"
-        constraints.joint_constraints.append(joint_constraint)
-
-        self.group.set_path_constraints(constraints)
-
-        return True
-
-    def remove_constraint(self, msg):
-        self.group.set_path_constraints(None)
-
-        return False
-
-    def test(self, msg):
-        pose_stamped = PoseStamped()
-        pose_stamped.header.frame_id = 'base_link'
-        pose_stamped.pose = msg.Pose
-
-        ik_request = PositionIKRequest() 
-        ik_request.group_name = 'manipulator'
-        ik_request.pose_stamped = pose_stamped
-        ik_request.timeout.secs = 0.1
-        ik_request.avoid_collisions = True
-
-        ik = rospy.ServiceProxy('/compute_ik', GetPositionIK)
-        res = ik(ik_request)
-
-        print(res)
-        return True
 
 
 
