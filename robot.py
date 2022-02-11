adf2eefe4bb1e889a421edde1405356cd1006739
Maleex99 <72606947+Maleex99@users.noreply.github.com>
2022-02-11 09:01:06 +0100

    Debogage

---
+++ robot.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+import rospkg
+import rospy
+from useful_robot import get_fk, pose_msg_to_homogeneous_matrix
+from run_qualite import run_qualite
+from run_identification import run_identification
+from deplacement_robot.msg import Identification, Qualite, Localisation
+from std_msgs.msg import Bool, String
+from deplacement_robot.srv import Robot_set_state
+import moveit_commander
+import sys
+import time
+
+import numpy as np
+
+class Robot:
+    def __init__(self):
+        self.plaque_pos = None
+        self.nom_plaque = None
+        self.intrinsic = None
+        self.H = None
+        self.image_global = None # TODO
+        self.moveit_commander = moveit_commander.roscpp_initialize(sys.argv)
+
+        rospack = rospkg.RosPack()
+        self.step_folder = rospack.get_path("deplacement_robot") + "/plaques"
+
+        self.pub_result = rospy.Publisher("result", Bool, queue_size=10)
+        self.pub_identification = rospy.Publisher("result/identification", Identification, queue_size=10)
+        self.pub_qualite = rospy.Publisher("result/qualite", Qualite, queue_size=10)
+        self.pub_localisation = rospy.Publisher("result/localisation", Localisation, queue_size=10)
+
+        self.srv_set_robot_state = rospy.ServiceProxy("set_robot_state", Robot_set_state)
+
+        rospy.Subscriber("/result/ok", Bool, self.result_aquitement)
+
+        self.aquitement = False
+
+    # Fonction de callback qui recoit l aquitement
+    def result_aquitement(self, msg):
+        self.aquitement = True
+
+    # Fonction pour envoyer un message jusqu a aquitement
+    def spam_result(self, pub, msg):
+        self.aquitement = False
+        rate = rospy.Rate(10)
+        while not self.aquitement and not rospy.is_shutdown():
+			print("spam")
+			pub.publish(msg)
+
+    # Fonction pour changer l etat de la production
+    def set_robot_state(self, state):
+        self.srv_set_robot_state(state)
+
+    def fin_prod(self):
+        group = moveit_commander.MoveGroupCommander("manipulator")
+
+        parcking = group.get_named_target_values("parcking")
+        keys = parcking.keys()
+
+        parcking_list = []
+        for k in sorted(keys):
+            parcking_list.append(parcking[k])        
+
+        parcking_list = np.round(parcking_list, 3)
+        curent_state = np.round(group.get_current_joint_values(), 3)
+
+	print(parcking_list)
+	print(curent_state)
+	print()
+
+        if np.linalg.norm(np.array(parcking_list) - np.array(curent_state)) <= 0.2:
+            self.set_robot_state("LIBRE INIT")
+        else:
+            self.set_robot_state("LIBRE NON INIT")
+
+
+
+    # Fonction pour lancer la phase de calibration
+    def execute_calibration(self):
+        # TODO : self.intrinsic = run_calibration()
+
+        return True
+
+    # Fonction pour lancer la phase de localisation
+    def execute_localisation(self, nom_plaque, send_result=True):
+        self.nom_plaque = nom_plaque
+
+        #TODO : msg,self.H = run_localisation()
+        msg = Localisation()
+        msg.x = float(0.55)
+        msg.y = float(0.24)
+        msg.z = float(0.005)
+        msg.a = float(0)
+        msg.b = float(0)
+        msg.g = float(0)
+        self.plaque_pos = np.array([[1,0,0,0.55],
+                                    [0,1,0,0.24],
+                                    [0,0,1,0.005],
+                                    [0,0,0,1]])
+
+	time.sleep(1)
+
+        if send_result:
+            self.pub_result.publish(True)
+            self.spam_result(self.pub_localisation, msg)
+
+        return True
+
+    # Fonction pour lancer la phase d identication
+    def execute_identification(self, nom_plaque, diametres, send_result=True):
+        if self.plaque_pos is None:
+            return False
+        '''if not self.param_int:
+            return False'''
+        if self.nom_plaque != nom_plaque:
+            self.execute_localisation(nom_plaque, send_result=False)
+
+        msg,_ = run_identification(self.plaque_pos, nom_plaque, self.step_folder, diametres) #TODO get image global
+
+        if send_result:
+            self.pub_result.publish(True)
+            self.spam_result(self.pub_identification, msg)
+
+        return True
+
+    # Fonction pour lancer la phase de qualites
+    def execute_qualite(self, nom_plaque, diametres):
+        if self.plaque_pos is None:
+            return False
+        if self.nom_plaque != nom_plaque:
+            self.execute_localisation(nom_plaque, send_result=False)
+            self.execute_identification(nom_plaque, diametres, send_result=False)
+        
+        msg = run_qualite(self.plaque_pos, nom_plaque, self.step_folder, diametres=diametres)
+
+        self.pub_result.publish(True)
+        self.spam_result(self.pub_qualite, msg)
+
+        return True
+
+
+
+
+
---
+++ run_identification.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+from StepReader import StepReader
+from useful_robot import rotation_between_vect, homogeneous_matrix_to_pose_msg, get_fk, pose_msg_to_homogeneous_matrix
+import numpy as np
+import rospy
+import rospkg
+from deplacement_robot.srv import Robot_move, Robot_move_predef, Robot_set_state
+from communication.srv import identification
+from deplacement_robot.msg import Identification, Trou_identification
+from geometry_msgs.msg import Pose
+
+from cv_bridge import CvBridge
+
+import matplotlib.pyplot as plt
+
+def run_identification(plaque_pos, nom_plaque, step_folder, diametres, dist =  1.03, seuil=100):
+    # Lecture du fichier step et recuperation des trous
+    cylinders_dict = get_cylinders(step_folder + "/" + str(nom_plaque) + ".stp")
+
+    # Determination du chemin en fonction des trous
+    poses = get_poses(cylinders_dict, plaque_pos, dist)
+
+    ######## Deplacement du robot ########
+    # Service pour deplacer le robot a un point donne
+    move_robot = rospy.ServiceProxy('move_robot', Robot_move)
+    # Service pour deplacer le robot a sa position de parcking
+    move_parcking = rospy.ServiceProxy('move_robot_parcking', Robot_move_predef)
+    # Service pour effectuer l identification
+    identification_srv = rospy.ServiceProxy("camera/identification", identification)
+
+    # Dictionnaire point 3D (x,y,z) : diametre
+    dict_point = {}
+
+    res_points = []
+    res_image_originale = []
+    res_image_annotee = []
+    decalage = 300
+    
+    bridge = CvBridge()
+
+    type_plaque = "Plate"
+
+    if len(poses) > 1:
+        type_plaque = "Courbee"
+
+    for pose in poses:
+        if rospy.is_shutdown():
+            exit()
+        move_parcking()
+        res_move = move_robot(pose)
+
+        if not res_move.res :
+            rospy.logwarn("Point inategnable. Passage au point suivant.")
+            res_points.append([])
+            res_image_originale.append(np.zeros((1944,decalage,3), dtype=np.uint8))
+            res_image_annotee.append(np.zeros((1944,decalage,3), dtype=np.uint8))
+        else:
+            res_identification = identification_srv(str(type_plaque))
+            res_points.append(res_identification.points.points)
+            res_image_originale.append(bridge.imgmsg_to_cv2(res_identification.originale, 'bgr8'))
+            res_image_annotee.append(bridge.imgmsg_to_cv2(res_identification.annotee, 'bgr8'))
+    
+    move_parcking()
+
+    image_annotee = np.concatenate(res_image_annotee,axis=1)
+    image_originale = np.concatenate(res_image_originale,axis=1)
+
+    points_msg = []
+    trous = []
+
+    for i,points in enumerate(res_points):
+        for point in points:
+            if rospy.is_shutdown():
+                exit()
+
+            p = Trou_identification()
+            p.x = point.x
+            p.y = point.y + i * decalage
+            p.diametre = point.type
+
+            trous.append((p.x,p.y))
+
+    msg = Identification()
+    msg.trous = points_msg
+
+    msg.image = bridge.cv2_to_compressed_imgmsg(image_annotee)
+
+    plt.imshow(image_annotee)
+    plt.draw()
+    plt.pause(0.001)
+    print("press enter")
+    raw_input()
+
+    return msg, (image_originale, trous)
+
+def get_points_projection(intrinsic, extrinsic, P0):
+    # [xi,yi,1] = 1/z*[intrinseque].[extrinseque].[point]
+    P0 = np.hstack((P0, 1))
+    Pc = np.dot(extrinsic,P0)
+    Pi = 1/Pc[2]*np.dot(intrinsic, Pc[:3])
+    pass
+
+def get_poses(cylinders_dict, plaque_pos, dist):
+    poses = []
+    poses_d = {}
+
+    for key in cylinders_dict:
+        cylinders = cylinders_dict[key]
+        point = []
+        for c in cylinders :
+            point.append(c.position)
+
+        m = np.mean(point, 0)/1000
+        m = np.dot(plaque_pos, np.hstack((m, 1)))[:3]
+
+        p1 = m + dist * np.array(key)
+        p2 = m - dist * np.array(key)
+
+        v = np.array(key)
+        p = None
+
+        if p2[2] > p1[2]:
+            p = np.array([p2[:3]])
+        else :
+            v = -v
+            p = np.array([p1[:3]])
+
+        r = get_orientation_mat(v)
+
+        pt = np.hstack((r, p.T))
+        pt = np.vstack((pt, [0,0,0,1]))
+        msg = homogeneous_matrix_to_pose_msg(pt)
+
+        poses_d[tuple(p[0])] = msg
+        
+    keys = poses_d.keys()
+    for key in sorted(list(keys)):
+        poses.append(poses_d[key])
+
+    return poses
+
+def get_cylinders(file_path):
+    step = StepReader(file_path)
+    cylinders_dict = {}
+    
+    for c in step.getCylinders():
+        if c.rayon <= 10:
+            l = cylinders_dict.get(c.direction, [])
+            l.append(c)
+            cylinders_dict[c.direction] = l
+
+    return cylinders_dict
+
+def get_orientation_mat(tz):
+    tz = tz / np.linalg.norm(tz)
+    tx = np.cross(tz,[0,1,0])
+    tx = tx / np.linalg.norm(tx)
+    ty = np.cross(tz, tx)
+    ty = ty / np.linalg.norm(ty)
+
+    R = np.hstack((np.array([tx]).T, np.array([ty]).T, np.array([tz]).T))
+    print(R)
+
+    return R
+
+if __name__ == "__main__":
+    rospy.init_node('test_identification', anonymous=True)
+    R = np.eye(4)
+    R[0,3] = 0.55
+    R[1,3] = 0.24
+    R[2,3] = -0.270 + 0.275
+    rospack = rospkg.RosPack()
+    cwd = rospack.get_path("deplacement_robot")
+    run_identification(R, "Plaque_2", cwd + "/plaques", [5,7,12,18])
