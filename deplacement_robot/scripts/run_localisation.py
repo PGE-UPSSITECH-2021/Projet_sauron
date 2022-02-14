c2bcfb8b9397aac715b0aa5fc6a1e75e03314032
oualidabda <o.elabdaoui@gmail.com>
2022-02-14 14:17:34 +0100

    implementation calib et préparation localisation

--- deplacement_robot/scripts/run_localisation.py
+++ deplacement_robot/scripts/run_localisation.py
@@ -5,6 +5,8 @@ from deplacement_robot.srv import Robot_move_predef
 from deplacement_robot.msg import Localisation
 from communication.srv import capture
 from cv_bridge import CvBridge
+from useful_robot import pose_msg_to_homogeneous_matrix,get_fk
+from pyquaternion import Quaternion
 
 class UntrustworthyLocalisationError(Exception):
     def __init__(self,message):
@@ -29,15 +31,9 @@ def main_loc():
     pass
 
 def move_to_point(p):
-    if p == 1:
-        move_robot = rospy.ServiceProxy('localisation_1', Robot_move_predef)
-        move_robot()
-        print("moving to point 1")
-    else :
-        move_robot = rospy.ServiceProxy('localisation_2', Robot_move_predef)
-        move_robot()
-        print("moving to point 2")
-
+    move_robot = rospy.ServiceProxy('localisation_'+str(p), Robot_move_predef)
+    move_robot()
+    print("moving to point "+str(p))
 
 
 def send_results(pts):
@@ -63,25 +59,18 @@ def get_image():
 
 
 def run_localisation():
-    #bouger au point prédéfinit 1
-    move_to_point(1)
-
+    #bouger au points prédéfinits
+    for i in range(4):
+        move_to_point(i+1)
     #capturer l'image
-    img = get_image()
-
+        img = get_image()
     #catch les exceptions (retour de mauvais type de donnée)
-    try:
-        pts,matrice_extr = localiser(type_plaque=,model_path=,image=img,M_hom_3D=,M_pass_oc=,M_intr=,coeff_distr=)
-    except TypeError:
-        print("plaque non trouvée")
-        #bouger au deuxième point prédéfinit
-        move_to_point(2)
-    
-    try:
-        pts,matrice_extr = localiser(type_plaque=,model_path=,image=,M_hom_3D=,M_pass_oc=,M_intr=,coeff_distr=)
-    except TypeError:
-        print("plaque non trouvée")
-
+        pose_get_fk = get_fk()
+        Mtrix_hom_3D = pose_msg_to_homogeneous_matrix(pose_get_fk)
+        try:
+            pts,matrice_extr = localiser(type_plaque=,model_path=,image=img,M_hom_3D=Mtrix_hom_3D,M_pass_oc=,M_intr=,coeff_distr=)
+        except TypeError:
+            print("plaque non trouvée")
 
 
 if __name__ == "__main__":
