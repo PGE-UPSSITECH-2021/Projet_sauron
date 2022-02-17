f2f0013ce5894b38c0217ac43aab6fc9cf3916a4
Alexandre Malgouyres <amalgouyres@gmail.com>
2022-02-17 16:45:38 +0100

    Correctif node_securite

--- deplacement_robot/scripts/node_securite.py
+++ deplacement_robot/scripts/node_securite.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 
 import rospy
-from industrial_msgs.msgs import RobotStatus 
+from industrial_msgs.msg import RobotStatus
+from std_msgs.msg import String
 
-global status
-status = "NOK"
 
-def callback_robot_status(msg):
-    if msg.e_stopped or msg.in_error :
-        status = "NOK"
-    else :
-        status = "OK"
-    
+class Secu:
+    def __init__(self):
+        self.status = "NOK"
+        self.pub_secu = rospy.Publisher("/securite_state", String, queue_size=10)
+        rospy.init_node('move_robot_server', anonymous=True)
+        rospy.Subscriber("/robot_status", RobotStatus, self.callback_robot_status)
 
-pub_secu = rospy.Publisher("securite_state", String, queue_size=10)
-rospy.init_node('move_robot_server', anonymous=True)
-rospy.Subscriber("/robot_status", RobotStatus, callback_robot_status)
+    def callback_robot_status(self,msg):
+        if msg.e_stopped.val != 0 or msg.in_error.val != 0:
+            self.status = "NOK"
+        else :
+            self.status = "OK"
+	    
+    def run(self):
+        rate = rospy.Rate(10)
 
-rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.pub_secu.publish(self.status)
+            rate.sleep()
 
-while not rospy.is_shutdown():
-    pub_secu.publish(status)
-    rate.sleep()
+
+s = Secu()
+s.run()
