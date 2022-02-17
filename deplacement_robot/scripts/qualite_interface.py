1810b00d8231269ddef988c63b5ad60ee76f0ce7
Ugo31 <36738570+Ugo31@users.noreply.github.com>
2022-02-17 15:04:23 +0100

    adding anti crash mechanism

--- deplacement_robot/scripts/qualite_interface.py
+++ deplacement_robot/scripts/qualite_interface.py
@@ -3,68 +3,78 @@ from qualite_pretraitement import *
 import cv2
 import time
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 def fonction_qualite(rayon_attendu,image_raw,debug,showResult = True,fast_algo= True):
-    #calcul a la main pas fiable.
-    start_time = time.time()
+    try:
+	    #calcul a la main pas fiable.
+	    start_time = time.time()
 
-    px_to_mm = 0.04624277456
-    mm_to_px = 21
-    rayon_attendu_px = int(rayon_attendu*mm_to_px*2)  #ici on bosse en px
-    if(fast_algo):	        
-        #if fast we scale down the img
-        height, width, _ = image_raw.shape 
-        scale = 5
-        rayon_attendu_px=int(rayon_attendu_px/scale)
-        dsize = (int(width/scale), int(height/scale))
-        image_raw = cv2.resize(image_raw, dsize)
-
-
-    if(debug):
-        cv2.imshow('RAW',cv2.resize(image_raw,(520,388)))
-        cv2.waitKey(0)
+	    px_to_mm = 0.04624277456
+	    mm_to_px = 21
+	    rayon_attendu_px = int(rayon_attendu*mm_to_px*2)  #ici on bosse en px
+	    if(fast_algo):	        
+		#if fast we scale down the img
+		height, width, _ = image_raw.shape 
+		scale = 5
+		rayon_attendu_px=int(rayon_attendu_px/scale)
+		dsize = (int(width/scale), int(height/scale))
+		image_raw = cv2.resize(image_raw, dsize)
 
 
-    image_processed,offset_,nohole=filtrage.preprocess(image_raw,fast_algo)
-	
-    if(nohole):
-        resized_image = cv2.resize(image_raw,(520,388))
-        cv2.circle(resized_image,(int(520/2),int(388/2)),50,(255,0,0),5) #dessine cercle
-        if(debug or showResult):
-            plt.imshow(resized_image)
-            plt.draw()
-            plt.pause(0.001)        
-        return True,"Trou non detecte",resized_image
-	
-
-    if(debug):
-        cv2.imshow('FILTERED',image_processed)
-        cv2.waitKey(0)
+	    if(debug):
+		cv2.imshow('RAW',cv2.resize(image_raw,(520,388)))
+		cv2.waitKey(0)
 
 
+	    image_processed,offset_,nohole=filtrage.preprocess(image_raw,fast_algo)
 
-    isdefective, defect, contours = analyseContour.caracterization(cv2.cvtColor(image_processed,cv2.COLOR_GRAY2RGB),rayon_attendu_px,px_to_mm,affichage=False)
-    
-    px_size = 15
-    if(fast_algo):
-        px_size = 5
+	    if(nohole):
+		resized_image = cv2.resize(image_raw,(520,388))
+		cv2.circle(resized_image,(int(520/2),int(388/2)),50,(255,0,0),5) #dessine cercle
+		if(debug or showResult):
+		    plt.imshow(resized_image)
+		    plt.draw()
+		    plt.pause(0.001)        
+		return True,"Trou non detecte",resized_image
 
-    assert(contours is not None)
 
-    print("image raw")
-    if(isdefective):
-        image_result = cv2.drawContours(image_raw, contours, -1, (255,0,0), px_size,offset=offset_)
-    else:
-        image_result= cv2.drawContours(image_raw, contours, -1, (0,255,0), px_size,offset=offset_)
-    
-    
-    if(showResult):
-        print("["+str((time.time() - start_time))+" seconds]")
-        plt.imshow(cv2.resize(image_result,(520,388)))
-        plt.draw()
-        plt.pause(0.001)
+	    if(debug):
+		cv2.imshow('FILTERED',image_processed)
+		cv2.waitKey(0)
 
-    return not isdefective, defect, image_result
+
+
+	    isdefective, defect, contours = analyseContour.caracterization(cv2.cvtColor(image_processed,cv2.COLOR_GRAY2RGB),rayon_attendu_px,px_to_mm,affichage=False)
+
+	    px_size = 15
+	    if(fast_algo):
+		px_size = 5
+
+	    assert(contours is not None)
+
+	    print("image raw")
+	    if(isdefective):
+		image_result = cv2.drawContours(image_raw, contours, -1, (255,0,0), px_size,offset=offset_)
+	    else:
+		image_result= cv2.drawContours(image_raw, contours, -1, (0,255,0), px_size,offset=offset_)
+
+
+	    if(showResult):
+		print("["+str((time.time() - start_time))+" seconds]")
+		plt.imshow(cv2.resize(image_result,(520,388)))
+		plt.draw()
+		plt.pause(0.001)
+
+	    return not isdefective, defect, image_result
+    except:
+	print("================================================================")
+	print("==================| QUALITE CRITICAL FAILURE |==================")
+	print("================================================================")
+	now = datetime.now()
+	date_time = now.strftime("ERROR_%m_%d_%Y__%H_%M_%S")
+	cv2.imwrite("./"+date_time+".png", image_raw)
+	return True,True,image_raw
 
 ######################################
 
