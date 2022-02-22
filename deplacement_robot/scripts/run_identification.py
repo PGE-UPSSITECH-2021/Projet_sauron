3c67afe2ce9aa5a9dfb3897fb9a30ca5d7830179
Alexandre Malgouyres <amalgouyres@gmail.com>
2022-02-22 11:36:14 +0100

    Ajout commentatire

--- deplacement_robot/scripts/run_identification.py
+++ deplacement_robot/scripts/run_identification.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# coding: utf-8
+
 from StepReader import StepReader
 from useful_robot import rotation_between_vect, homogeneous_matrix_to_pose_msg, get_fk, pose_msg_to_homogeneous_matrix
 import numpy as np
@@ -15,6 +17,7 @@ import cv2
 
 import matplotlib.pyplot as plt
 
+# Fonction main pour l'identification des trous
 def run_identification(plaque_pos, nom_plaque, step_folder, diametres, intrinsec, pos_outil_cam, dist =  1.03, seuil=100, pub = None):
     pub_state(pub, "Debut identification")
     pub_state(pub, "Calcul de la trajectoire")
@@ -41,7 +44,7 @@ def run_identification(plaque_pos, nom_plaque, step_folder, diametres, intrinsec
     res_points = []
     res_image_originale = []
     res_image_annotee = []
-    decalage = 300
+    decalage = 300  # Decalage pour la reconstruction de l'image globale lorsqu'elle est constitué de plusieur prise de vue
     
     bridge = CvBridge()
 
@@ -54,9 +57,11 @@ def run_identification(plaque_pos, nom_plaque, step_folder, diametres, intrinsec
     # Deroulement de la trajectoire et de l'identification
     nbPose = len(poses)
     point_3D_2D = {}
-    for i,pose in enumerate(poses):
-        if rospy.is_shutdown():
+    for i,pose in enumerate(poses): # Action pour chaque position de la trajectoire
+        if rospy.is_shutdown(): # Vérification que ROS n'a pas reçu de demande d'arrêt
             exit()
+
+        # Déplacement du robot à la position de prise d'image
         pub_state(pub, "Deplacement a la position " + str(i+1) + "/" + str(nbPose))
         move_parking()
         res_move = move_robot(pose[0])
@@ -65,25 +70,24 @@ def run_identification(plaque_pos, nom_plaque, step_folder, diametres, intrinsec
             pub_state(pub, "Position inatteignable. Passage au position suivante")
             rospy.logwarn("Point inatteignable. Passage au point suivant.")
             res_points.append([])
-            res_image_originale.append(np.zeros((1944,decalage,3), dtype=np.uint8))
+            res_image_originale.append(np.zeros((1944,decalage,3), dtype=np.uint8)) # Ajout d'une image vide dans l'image globale si la position de prise de vue est inatteignaible
             res_image_annotee.append(np.zeros((1944,decalage,3), dtype=np.uint8))
         else:
             pub_state(pub, "Identification des trous")
             res_identification = identification_srv(diametres, str(type_plaque))
             res_points.append(res_identification.points.points)
-            res_image_originale.append(bridge.imgmsg_to_cv2(res_identification.originale, 'bgr8'))
+            res_image_originale.append(bridge.imgmsg_to_cv2(res_identification.originale, 'bgr8')) # Concaténation des images pour l'image des résultats
             res_image_annotee.append(bridge.imgmsg_to_cv2(res_identification.annotee, 'bgr8'))
 
         points_im = []
-        for p in res_identification.points.points:
+        for p in res_identification.points.points:  # Récupération des points pour le matching 2D 3D de l'image globale
             points_im.append((p.x, p.y))
 
-        pos_cam = pose_msg_to_homogeneous_matrix(get_fk())
-        #TODO
-        pos_outil_cam = np.array([[0,-1,0,-0.035],[1,0,0,-0.035],[0,0,1,0.05],[0,0,0,1]])
-        point_3D_2D = projection_3D_2D(pose[1], pos_cam, pos_outil_cam, plaque_pos, intrinsec, points_im, decalage*i, point_3D_2D)
+        pos_cam = pose_msg_to_homogeneous_matrix(get_fk()) # Récupération de l'image du robot
+        point_3D_2D = projection_3D_2D(pose[1], pos_cam, pos_outil_cam, plaque_pos, intrinsec, points_im, decalage*i, point_3D_2D) #Maching des points 2D et 3D
 
-        img = np.concatenate(res_image_originale,axis=1)
+        #DEBUG
+        '''img = np.concatenate(res_image_originale,axis=1)
 
         for P_3D in point_3D_2D:
             p = point_3D_2D[P_3D]
@@ -91,19 +95,20 @@ def run_identification(plaque_pos, nom_plaque, step_folder, diametres, intrinsec
 
         img = cv2.resize(img, (img.shape[1]/3, img.shape[0]/3))
         cv2.imshow("Result", img)
-        cv2.waitKey(0)
+        cv2.waitKey(0)'''
     
     pub_state(pub, "Identification finie, retour position parking")
 
-    move_parking()
+    move_parking()  # Retour position de parking
 
+    # Création des images de résultats
     image_annotee = np.concatenate(res_image_annotee,axis=1)
     image_originale = np.concatenate(res_image_originale,axis=1)
 
-    points_msg = []
-
     pub_state(pub, "Traitement des donnees")
 
+    # Création du message ROS
+    points_msg = []
     for i,points in enumerate(res_points):
         for point in points:
             if rospy.is_shutdown():
@@ -122,20 +127,23 @@ def run_identification(plaque_pos, nom_plaque, step_folder, diametres, intrinsec
 
     msg.image = bridge.cv2_to_compressed_imgmsg(image_annotee)
 
-    plt.imshow(image_annotee)
+    #DEBUG
+    '''plt.imshow(image_annotee)
     plt.draw()
-    plt.pause(0.001)
+    plt.pause(0.001)'''
     '''print("press enter")
     raw_input()'''
     pub_state(pub, "Identification terminee")
 
-    return msg, image_originale, point_3D_2D
+    return msg, image_originale, point_3D_2D # message ROS, l'image globale, matching point 3D et 2D image
 
 def pub_state(pub, msg):
+    # Fonction pour transmettre l'étape de la production
     if not pub is None:
         pub.publish(msg)
 
 def get_points_projection(intrinsic, extrinsic, p):
+    # Fonction pour projeter les points 3D sur l'image 2D
     # [xi,yi,1] = 1/z*[intrinseque].[extrinseque].[point]
     P_monde = np.hstack((p, 1))
     P_camera = np.dot(extrinsic, P_monde)
@@ -144,6 +152,9 @@ def get_points_projection(intrinsic, extrinsic, p):
 
 
 def projection_error(projection, Liste2D):
+    # projection = Point projeté calculé
+    # Liste2D = Point calculer avec l'identification
+    # Calcul de l'erreur moyenne de la projection de points 3D sur l'image
     erreurs = []
 
     for p in projection:
@@ -155,6 +166,7 @@ def projection_error(projection, Liste2D):
     return np.mean(erreurs,axis=0)
 
 def appariement(proj, P_3D, err, decY, point_3D_2D):
+    # Fonction pour faire le lien en les point 3D et leur position sur l'image resultat {3D:2D}
     for i,p in enumerate(proj):
         p[:1] += decY
         p = np.array(p) - err
@@ -163,47 +175,51 @@ def appariement(proj, P_3D, err, decY, point_3D_2D):
     return point_3D_2D
 
 def projection_3D_2D(Liste3D, pos_monde_outil, pos_outil_cam, pos_plaque, intrinsic, Liste2D, decY, point_3D_2D):
-    pos_monde_cam = np.dot(pos_monde_outil, pos_outil_cam)
+    # Fonction pour calculer la position des points 3D sur l'image resultat {3D:2D}
+    pos_monde_cam = np.dot(pos_monde_outil, pos_outil_cam)  # Calcul de la matrice de passare monde -> caméra
     # Convertion metre to millimetre
     pos_monde_cam[:3,3] = pos_monde_cam[:3,3] * 1000
     pos_monde_plaque = np.array(pos_plaque)
     pos_monde_plaque[:3,3] = pos_monde_plaque[:3,3] * 1000
 
-    extrinsic = np.linalg.inv(pos_monde_cam)    
+    extrinsic = np.linalg.inv(pos_monde_cam)    # Calcul des paramètres extrinsèque
 
     points_3D = []
     P_mondes = []
-    for p in Liste3D:
+    for p in Liste3D:   # Passage des points du repère plaque vers le repère monde
         P_plaque = np.hstack((p, 1))
         P_monde = np.dot(pos_monde_plaque, P_plaque)
         P_mondes.append(P_monde[:3])
         points_3D.append(P_monde[:3])
 
     pos2D = []
-    for x, y, z in points_3D:
+    for x, y, z in points_3D:   # Projection des points dans le repère image
         proj = get_points_projection(intrinsic, extrinsic, [x,y,z])
         pos2D.append(proj)
 
-    err = projection_error(pos2D, Liste2D)
-    point_3D_2D = appariement(pos2D, P_mondes, err, decY, point_3D_2D)
+    err = projection_error(pos2D, Liste2D) # Calcul de l'erreur de projection
+    point_3D_2D = appariement(pos2D, P_mondes, err, decY, point_3D_2D) # Matchind 3D 2D
     
     return point_3D_2D
 
 def get_poses(cylinders_dict, plaque_pos, dist):
+    # Calcul de la position de chaque position de prise de vue
     poses = []
     poses_d = {}
 
+    # pour chaque liste de trous avec la même normal
     for key in cylinders_dict:
         cylinders = cylinders_dict[key]
         point = []
         point_m = []
-        for c in cylinders :
+        for c in cylinders : # on converti tous les trous en metre
             point.append(np.array(c.position)/1000)
             point_m.append(c.position)
 
-        m = np.mean(point, 0)
+        m = np.mean(point, 0)   # calcul de la position du centre des trous
         m = np.dot(plaque_pos, np.hstack((m, 1)))[:3]
 
+        # calcul de la position du robot en fonction de point central et de la normal
         p1 = m + dist * np.array(key)
         p2 = m - dist * np.array(key)
 
@@ -216,14 +232,18 @@ def get_poses(cylinders_dict, plaque_pos, dist):
             v = -v
             p = np.array([p1[:3]])
 
+        # passage d'un vecteur de direction vers une matrice de rotation
         r = get_orientation_mat(v)
 
+        # calcul de la matrice de passage homogène
         pt = np.hstack((r, p.T))
         pt = np.vstack((pt, [0,0,0,1]))
+        # Création du message ROS pour le déplacement du robot
         msg = homogeneous_matrix_to_pose_msg(pt)
 
         poses_d[tuple(p[0])] = (msg, point_m)
-        
+    
+    # Calcul de l'autre de passage pour chaque position
     keys = poses_d.keys()
     for key in sorted(list(keys)):
         poses.append(poses_d[key])
@@ -231,6 +251,7 @@ def get_poses(cylinders_dict, plaque_pos, dist):
     return poses
 
 def get_cylinders(file_path):
+    # Récupération des cylindres présent sur le modèle CAO
     step = StepReader(file_path)
     cylinders_dict = {}
     
@@ -243,6 +264,7 @@ def get_cylinders(file_path):
     return cylinders_dict
 
 def get_orientation_mat(tz):
+    # Passage d'un vecteur directeur vers une matrice de rotation
     tz = tz / np.linalg.norm(tz)
     tx = np.cross(tz,[0,1,0])
     tx = tx / np.linalg.norm(tx)
@@ -254,6 +276,7 @@ def get_orientation_mat(tz):
     return R
 
 if __name__ == "__main__":
+    # main de test de l'identification
     rospy.init_node('test_identification', anonymous=True)
     R = np.eye(4)
     R[0,3] = 0.55
